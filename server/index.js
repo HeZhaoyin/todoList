8f7c3e0a09c0333b000c21faa2d0439f4c2ae309
hezhaoyin <2561033753@qq.com>
2018-07-25 16:59:08 +0800

    完成token验证

--- server/index.js
+++ server/index.js
@@ -1,22 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const jwt = require('jwt-simple')
-const app = express()
+const app = global.app =  express()
 const Users = require('./db/model/user')
 const jwtauth = require('./user/auth')
 app.set('jwtTokenSecret', 'HEZHAOYINTODOLIST');
-app.use(bodyParser.json())
+
 var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*'); //自定义中间件，设置跨域需要的响应头。
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With,X-Access-Token");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   if (req.method == 'OPTIONS') {
-    res.send(200) // 快速响应OPIONTS
+    res.status(200).send('ok') // 快速响应OPIONTS
   }
   next();
 };
 
 app.use(allowCrossDomain); //运用跨域的中间件
+app.use(bodyParser.json())
 
 app.route('/user/:username').get((req, res) => {
   Users.find({
@@ -38,14 +39,16 @@ app.route('/user/:username').get((req, res) => {
   })
 })
 app.route('/user/:username/:password').get((req, res) => {
+  console.log('123');
   Users.find({
     username: req.params.username,
     password: req.params.password
   }, (err, user) => {
     if (user.length !== 0) {
       var expires = Date.now() + 7 * 24 * 3600 * 1000;
+      console.log(user)
       var token = jwt.encode({
-        iss: user.id,
+        iss: user[0]._id,
         exp: expires
       }, app.get('jwtTokenSecret'));
       res.json({
@@ -53,7 +56,7 @@ app.route('/user/:username/:password').get((req, res) => {
         message: '登录成功，欢迎您：' + req.params.username,
         token: token,
         expires: expires,
-        user: user
+        user: user[0]
       });
       return
     } else {
@@ -103,16 +106,17 @@ app.route('/user').post((req, res) => {
     }
   })
 })
-app.get('/todoList', function (req, res) {
-  // if (req.user) {
+app.get('/todoList', jwtauth, function (req, res) {
+  console.log(req.user)
+  if (req.user) {
     res.json({
       success: true,
     })
-  // } else {
-  //   res.json({
-  //     success: false,
-  //   })
-  // }
+  } else {
+    res.json({
+      success: false,
+    })
+  }
 })
 
 app.listen(4000)
--- server/user/auth.js
+++ server/user/auth.js
@@ -2,22 +2,24 @@ const jwt = require('jwt-simple')
 const Users = require('../db/model/user')
 
 module.exports = function (req, res, next) {
-  var token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['X-Access-Token'];
-  console.log('token是' + token)
+  var token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token'];
   if (token) {
     try {
       var decoded = jwt.decode(token, app.get('jwtTokenSecret'));
+      console.log(decoded)
       if (decoded.exp <= Date.now()) {
         res.end('Access token has expired', 400);
       }
       Users.findOne({
         _id: decoded.iss
       }, function (err, user) {
+        console.log(user)
         req.user = user;
+        next();
       });
       // handle token here
-      next();
     } catch (err) {
+      console.log('错误在这里',err)
       return next();
     }
   } else {
